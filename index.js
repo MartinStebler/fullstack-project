2893618a1e9da8a74aeac0195d2b2cd414b31979
Martin Stebler <contact@martinstebler.ch>
2025-10-14 14:50:33 +0200

    add tailwind

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const { PrismaClient } = require('@prisma/client');
 
 const app = express();
@@ -11,7 +12,10 @@ const PORT = process.env.PORT || 3000;
 // Express setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
+app.set('layout', 'layout');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
