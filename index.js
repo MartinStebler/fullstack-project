bb05ce11c09272ee1aa6c6a8fb68ab9ba97f12fd
Martin Stebler <contact@martinstebler.ch>
2025-11-03 17:33:51 +0100

    remove edit/delete routes

--- index.js
+++ index.js
@@ -30,47 +30,12 @@ app.get('/posts', async (req, res) => {
   res.render('posts/index', { posts });
 });
 
-// New post form
-app.get('/posts/new', (req, res) => {
-  res.render('posts/new');
-});
-
-// Create post
-app.post('/posts', async (req, res) => {
-  const { title, content } = req.body;
-  await prisma.post.create({ data: { title, content } });
-  res.redirect('/posts');
-});
-
 // Show post
 app.get('/posts/:id', async (req, res) => {
   const id = Number(req.params.id);
   const post = await prisma.post.findUnique({ where: { id } });
   if (!post) return res.status(404).send('Post not found');
   res.render('posts/show', { post });
-});
-
-// Edit post form
-app.get('/posts/:id/edit', async (req, res) => {
-  const id = Number(req.params.id);
-  const post = await prisma.post.findUnique({ where: { id } });
-  if (!post) return res.status(404).send('Post not found');
-  res.render('posts/edit', { post });
-});
-
-// Update post (using POST to avoid method-override dependency)
-app.post('/posts/:id', async (req, res) => {
-  const id = Number(req.params.id);
-  const { title, content } = req.body;
-  await prisma.post.update({ where: { id }, data: { title, content } });
-  res.redirect(`/posts/${id}`);
-});
-
-// Delete post (using POST to avoid method-override dependency)
-app.post('/posts/:id/delete', async (req, res) => {
-  const id = Number(req.params.id);
-  await prisma.post.delete({ where: { id } });
-  res.redirect('/posts');
 });
 
 app.listen(PORT, () => {
